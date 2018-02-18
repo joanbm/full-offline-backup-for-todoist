3b8537e78d34a7b06c5e156b2f29047224db479a
Joan Bruguera <joanbrugueram@gmail.com>
2018-02-18 18:14:34 +0100

    Add code to fix Todoist's ZIP files with UTF-8 file names.

--- tests/test_todoist_backup_downloader.py
+++ tests/test_todoist_backup_downloader.py
@@ -2,6 +2,7 @@ import unittest
 import tempfile
 import shutil
 import os
+import zipfile
 from pathlib import Path
 from todoist_api import TodoistBackupInfo
 from todoist_backup_downloader import TodoistBackupDownloader
@@ -13,7 +14,9 @@ class TestTodoistApi(unittest.TestCase):
         self.__test_dir = tempfile.mkdtemp()
         self.__input_path = os.path.join(self.__test_dir, "input1.zip")
         self.__backup = TodoistBackupInfo("2016-01-01 12:30", "file://" + self.__input_path)
-        Path(self.__input_path).write_text("input1")
+
+        with zipfile.ZipFile(self.__input_path, "w") as _:
+            pass
 
     def tearDown(self):
         # Destroy sample filesystem structure
@@ -37,17 +40,67 @@ class TestTodoistApi(unittest.TestCase):
 
         # Assert
         self.assertEqual(dstpath, expected_dstpath)
-        self.assertEqual(Path(dstpath).read_text(), "input1")
+        self.assertTrue(zipfile.is_zipfile(expected_dstpath))
 
     def test_on_latest_version_and_existing_destionation_doesnt_overwrite(self):
         # Arrange
         backup_downloader = TodoistBackupDownloader(NullTracer())
         expected_dstpath = os.path.join(self.__test_dir, "2016-01-01 12_30.zip")
-        Path(expected_dstpath).write_text("i am untouchable")
+
+        with zipfile.ZipFile(expected_dstpath, "w") as _:
+            pass
 
         # Act
         dstpath = backup_downloader.download(self.__backup, self.__test_dir)
 
         # Assert
         self.assertEqual(dstpath, expected_dstpath)
-        self.assertEqual(Path(expected_dstpath).read_text(), "i am untouchable")
+        self.assertTrue(zipfile.is_zipfile(expected_dstpath))
+
+    def test_on_zip_with_no_utf8_flag_sets_flag(self):
+        # Arrange
+        backup_downloader = TodoistBackupDownloader(NullTracer())
+
+        # This is a prepared ZIP file with UTF-8 filenames but without the UTF-8 filenames set,
+        # like those that come from Todoist backups...
+        Path(self.__input_path).write_bytes(bytearray.fromhex(
+            '504b03040a03000000000b90524c0000000000000000000000000d00000041534349494e414d452e747874' + 
+            '504b03040a03000000000f90524c00000000000000000000000014000000554e49434f44454e414d4520f09f92a92e747874' + 
+            '504b01023f030a03000000000b90524c0000000000000000000000000d0024000000000000002080a4810000000041534349494e414d452e7478740a0020000000000001001800005773f6d9a8d301005773f6d9a8d301005773f6d9a8d301' +
+            '504b01023f030a03000000000f90524c000000000000000000000000140024000000000000002080a4812b000000554e49434f44454e414d4520f09f92a92e7478740a002000000000000100180080749ffad9a8d30180749ffad9a8d30180749ffad9a8d301' +
+            '504b05060000000002000200c50000005d0000000000'))
+
+        # Act
+        dstpath = backup_downloader.download(self.__backup, self.__test_dir)
+
+        # Assert
+        with zipfile.ZipFile(dstpath, "r") as zip_file:
+            self.assertEqual(zip_file.testzip(), None)
+            self.assertIn("ASCIINAME.txt", zip_file.namelist())
+            self.assertTrue(zip_file.getinfo("ASCIINAME.txt").flag_bits & 0x800)
+            self.assertIn("UNICODENAME 💩.txt", zip_file.namelist())
+            self.assertTrue(zip_file.getinfo("UNICODENAME 💩.txt").flag_bits & 0x800)
+
+    def test_on_zip_with_utf8_flag_does_not_break(self):
+        # Arrange
+        backup_downloader = TodoistBackupDownloader(NullTracer())
+
+        # This is a prepared ZIP file with UTF-8 filenames but without the UTF-8 filenames set,
+        # like those that come from Todoist backups...
+        Path(self.__input_path).write_bytes(bytearray.fromhex(
+            '504b03040a03000800000b90524c0000000000000000000000000d00000041534349494e414d452e747874' + 
+            '504b03040a03000800000f90524c00000000000000000000000014000000554e49434f44454e414d4520f09f92a92e747874' + 
+            '504b01023f030a03000800000b90524c0000000000000000000000000d0024000000000000002080a4810000000041534349494e414d452e7478740a0020000000000001001800005773f6d9a8d301005773f6d9a8d301005773f6d9a8d301' +
+            '504b01023f030a03000800000f90524c000000000000000000000000140024000000000000002080a4812b000000554e49434f44454e414d4520f09f92a92e7478740a002000000000000100180080749ffad9a8d30180749ffad9a8d30180749ffad9a8d301' +
+            '504b05060000000002000200c50000005d0000000000'))
+
+        # Act
+        dstpath = backup_downloader.download(self.__backup, self.__test_dir)
+
+        # Assert
+        with zipfile.ZipFile(dstpath, "r") as zip_file:
+            self.assertEqual(zip_file.testzip(), None)
+            self.assertIn("ASCIINAME.txt", zip_file.namelist())
+            self.assertTrue(zip_file.getinfo("ASCIINAME.txt").flag_bits & 0x800)
+            self.assertIn("UNICODENAME 💩.txt", zip_file.namelist())
+            self.assertTrue(zip_file.getinfo("UNICODENAME 💩.txt").flag_bits & 0x800)
--- todoist_backup_downloader.py
+++ todoist_backup_downloader.py
@@ -2,13 +2,74 @@
 """ Class to download Todoist backup ZIPs using the Todoist API """
 import os
 import urllib.request
+import zipfile
 import re
+from pathlib import Path
+import struct
 
 class TodoistBackupDownloader:
     """ Class to download Todoist backup ZIPs using the Todoist API """
+    __ZIP_FLAG_BITS_UTF8 = 0x800
 
     def __init__(self, tracer):
         self.__tracer = tracer
+
+    def __fixup_zip_utf8_flags(self, backup_zip_path):
+        """ Todoist backup ZIPs may contain filenames encoded in UTF-8, but they will not
+            actually have the UTF-8 filename flag set in the ZIP file.
+            This causes some ZIP parsers, such as Python's own parser, to consider the
+            file names in the legacy CP-437 format.
+            To fix this, we add the flag ourselves after downloading the ZIP. """
+        self.__tracer.trace("Fixing ZIP UTF-8 flags of '{}'...".format(backup_zip_path))
+
+        # Read the ZIP into a byte array in order to patch it...
+        # since Python's zipfile can't update the file without rebuilding
+        # it all from scratch
+        zipbytes = bytearray(Path(backup_zip_path).read_bytes())
+        modified = False
+
+        with zipfile.ZipFile(backup_zip_path, 'r') as zip_file:
+            for z_i in zip_file.infolist():
+                # If the flag is already set, nothing to do
+                if z_i.flag_bits & TodoistBackupDownloader.__ZIP_FLAG_BITS_UTF8:
+                    self.__tracer.trace("Local header @{} already OK".format(
+                        hex(z_i.header_offset)))
+                    continue
+
+                modified = True
+
+                # Patch the bits in the local file header. This is pretty easy,
+                # since zipfile tells us where it starts
+                self.__tracer.trace("Patching local header @{}".format(hex(z_i.header_offset)))
+                orig_local_flags = struct.unpack_from("<h", zipbytes, z_i.header_offset+6)[0]
+                struct.pack_into("<h", zipbytes, z_i.header_offset+6,
+                                 orig_local_flags | TodoistBackupDownloader.__ZIP_FLAG_BITS_UTF8)
+
+                # Patch the bits in the central file header. This is tricky,
+                # because Python doesn't tell us where it is!
+                # To avoid having parse the whole ZIP (which is a freaking mess),
+                # we build the part of the file that we have to patch from Python's data,
+                # and then find it in the actual file. This is very inefficient,
+                # but at least, it shouldn't fail under normal circumstances...
+                dosdate = (z_i.date_time[0] - 1980) << 9 | z_i.date_time[1] << 5 | z_i.date_time[2]
+                dostime = z_i.date_time[3] << 11 | z_i.date_time[4] << 5 | (z_i.date_time[5] // 2)
+                cd_header = struct.pack("<4s4B4HL2L", b"PK\001\002",
+                                        z_i.create_version, z_i.create_system,
+                                        z_i.extract_version, z_i.reserved,
+                                        z_i.flag_bits, z_i.compress_type, dostime, dosdate,
+                                        z_i.CRC, z_i.compress_size, z_i.file_size)
+                central_header_offset = zipbytes.find(cd_header)
+
+                self.__tracer.trace("Patching central header @{}".format(
+                    hex(central_header_offset)))
+                orig_local_flags = struct.unpack_from("<h", zipbytes, central_header_offset+8)[0]
+                struct.pack_into("<h", zipbytes, central_header_offset+8,
+                                 orig_local_flags | TodoistBackupDownloader.__ZIP_FLAG_BITS_UTF8)
+
+        if modified:
+            self.__tracer.trace("Writing back patched ZIP file...")
+            Path(backup_zip_path).write_bytes(zipbytes)
+
 
     def download(self, backup, output_path):
         """ Downloads the specified backup to the specified folder """
@@ -19,12 +80,16 @@ class TodoistBackupDownloader:
         sanitized_file_name = re.sub(r'[^a-zA-Z0-9_\- ]', "_", backup.version) + ".zip"
         output_file_path = os.path.join(output_path, sanitized_file_name)
 
+        # Download the file
         if not os.path.isfile(output_file_path):
             self.__tracer.trace("Downloading from {} to file '{}'...".format(
-                backup.url, sanitized_file_name))
+                backup.url, output_file_path))
             urllib.request.urlretrieve(backup.url, output_file_path)
         else:
             self.__tracer.trace("File '{}' already downloaded... skipping".format(
-                sanitized_file_name))
+                output_file_path))
+
+        # Fix UTF-8 flags
+        self.__fixup_zip_utf8_flags(output_file_path)
 
         return output_file_path
