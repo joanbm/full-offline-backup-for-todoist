60fe75d2803fc23428a6eaf59f8c0956034ed57f
Joan Bruguera <joanbrugueram@gmail.com>
2018-03-04 17:58:50 +0100

    Add additional info to setuptools file.

--- setup.py
+++ setup.py
@@ -3,4 +3,15 @@ setup(
     name="todoist-full-offline-backup",
     version="0.1",
     packages=find_packages(),
+
+    author="Joan Bruguera Micó",
+    author_email="joanbrugueram@gmail.com",
+    description="Small, dependency-less Python script to make a backup of all Todoist tasks and attachments that is accessible offline",
+    license="GPLv3",
+    keywords="todoist online backup attachments local full whole files",
+    url="https://github.com/joanbm/todoist-full-offline-backup",
+    project_urls={
+        "Bug Tracker": "https://github.com/joanbm/todoist-full-offline-backup/issues",
+        "Source Code": "https://github.com/joanbm/todoist-full-offline-backup",
+    }
 )
