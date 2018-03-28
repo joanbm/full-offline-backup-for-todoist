35cc3ce20661a7ff85decd04973165fb05928b32
Joan Bruguera <joanbrugueram@gmail.com>
2018-03-29 00:10:49 +0200

    Improve the URL downloader tests to make the HTTP server reusable.

--- todoist_full_offline_backup/tests/test_urldownloader.py
+++ todoist_full_offline_backup/tests/test_urldownloader.py
@@ -2,57 +2,26 @@
 """ Tests for the URL downloader """
 # pylint: disable=invalid-name
 import unittest
-from pathlib import Path
-import tempfile
-import http.server
-import socketserver
-import os
-import threading
-import shutil
+from .test_util_static_http_request_handler import TestStaticHTTPServer
 from ..url_downloader import URLLibURLDownloader
-
-class TestTCPServer(socketserver.TCPServer):
-    """ TCP server, which stores the last requested URL path (for assertions on it) """
-
-    def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True):
-        super(TestTCPServer, self).__init__(server_address, RequestHandlerClass, bind_and_activate)
-        self.last_requested_path = None
-
-class TestHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    """ HTTP Request handler, which stores the last requested URL path (for assertions on it) """
-
-    # pylint: disable=redefined-builtin
-    def log_message(self, format, *args):
-        # Disable console logging
-        pass
-
-    def do_GET(self):
-        self.server.last_requested_path = self.path
-        return super(TestHTTPRequestHandler, self).do_GET()
 
 class TestFrontend(unittest.TestCase):
     """ Tests for the URL downloader """
 
     def setUp(self):
-        """ Creates the sample filesystem structure and HTTP server for the test """
-        self.__test_dir = tempfile.mkdtemp()
-        self.__sample_file = os.path.join(self.__test_dir, "sample.txt")
-        Path(self.__sample_file).write_text("this is a sample")
+        """ Creates the sample HTTP server for the test """
 
         # Set up a quick and dirty HTTP server
-        os.chdir(self.__test_dir)
-        socketserver.TCPServer.allow_reuse_address = True
-        self.__httpd = TestTCPServer(("", 33327), TestHTTPRequestHandler)
-        self.__httpd_thread = threading.Thread(target=self.__httpd.serve_forever)
-        self.__httpd_thread.start()
+        route_responses = {
+            "/sample.txt": "this is a sample".encode("utf-8"),
+            "/sample.txt?param=value": "this is a sample with a parameter".encode("utf-8"),
+        }
+
+        self.__httpd = TestStaticHTTPServer(("127.0.0.1", 33327), route_responses)
 
     def tearDown(self):
-        """ Destroys the sample filesystem structure and HTTP server for the test """
+        """ Destroys the sample HTTP server for the test """
         self.__httpd.shutdown()
-        self.__httpd_thread.join()
-        self.__httpd.server_close()
-
-        shutil.rmtree(self.__test_dir)
 
     def test_urldownloader_can_download_local_file(self):
         """ Tests that the list backups operation prints the list of backups to the console """
@@ -63,7 +32,6 @@ class TestFrontend(unittest.TestCase):
         data = urldownloader.get("http://127.0.0.1:33327/sample.txt")
 
         # Assert
-        self.assertEqual(self.__httpd.last_requested_path, "/sample.txt")
         self.assertEqual(data.decode(), "this is a sample")
 
     def test_urldownloader_can_pass_request_params(self):
@@ -75,5 +43,12 @@ class TestFrontend(unittest.TestCase):
         data = urldownloader.get("http://127.0.0.1:33327/sample.txt", {'param': 'value'})
 
         # Assert
-        self.assertEqual(self.__httpd.last_requested_path, "/sample.txt?param=value")
-        self.assertEqual(data.decode(), "this is a sample")
+        self.assertEqual(data.decode(), "this is a sample with a parameter")
+
+    def test_urldownloader_throws_on_not_found(self):
+        """ Tests that the list backups operation prints the list of backups to the console """
+        # Arrange
+        urldownloader = URLLibURLDownloader()
+
+        # Act/Assert
+        self.assertRaises(Exception, urldownloader.get, "http://127.0.0.1:33327/notfound.txt")
---
+++ todoist_full_offline_backup/tests/test_util_static_http_request_handler.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Static mapping HTTP Server for the tests """
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+
+class TestStaticHTTPServer:
+    """ Static mapping HTTP Server for the tests """
+    def __init__(self, server_address, route_responses):
+        handler = TestStaticHTTPServer.make_test_http_request_handler(route_responses)
+        self.__httpd = HTTPServer(server_address, handler)
+        self.__httpd_thread = threading.Thread(target=self.__httpd.serve_forever)
+        self.__httpd_thread.start()
+
+    def shutdown(self):
+        """ Destroys the sample filesystem structure and HTTP server for the test """
+        self.__httpd.shutdown()
+        self.__httpd_thread.join()
+        self.__httpd.server_close()
+
+    @staticmethod
+    def make_test_http_request_handler(route_responses):
+        """ Creates an HTTP Request Handler class with a static
+            route mapping defined by the given parameter. """
+
+        class TestHTTPRequestHandler(BaseHTTPRequestHandler):
+            """ Static HTTP Request Handler class for the tests """
+
+            def log_message(self, format, *args): # pylint: disable=redefined-builtin
+                """ Disables console output for the HTTP Request Handler """
+                pass
+
+            def do_GET(self): # pylint: disable=invalid-name
+                """ Handles a request using the defined static mapping """
+                self.send_response(200 if self.path in route_responses else 404)
+
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+
+                if self.path in route_responses:
+                    self.wfile.write(route_responses[self.path])
+                return
+
+        return TestHTTPRequestHandler
