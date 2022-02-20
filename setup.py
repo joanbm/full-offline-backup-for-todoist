149c8a77d781efc71220fafdc533adbd71d5f926
Joan Bruguera <joanbrugueram@gmail.com>
2022-02-20 13:17:18 +0100

    Update version before release.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="full-offline-backup-for-todoist",
-    version="0.1",
+    version="0.4.2",
     packages=find_packages(),
 
     author="Joan Bruguera Micó",
