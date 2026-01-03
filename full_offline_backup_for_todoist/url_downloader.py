548ac3e3d19372fbb024fda7aaad2d01d176bbaf
Joan Bruguera Micó <joanbrugueram@gmail.com>
2026-01-03 16:40:14 +0000

    Fix ResourceWarning on Python 3.14 after URL download error.

--- full_offline_backup_for_todoist/url_downloader.py
+++ full_offline_backup_for_todoist/url_downloader.py
@@ -15,6 +15,9 @@ class _Request(NamedTuple):
     params: Optional[Dict[str, str]] = None
     data: Optional[Dict[str, str]] = None
 
+class URLDownloaderException(Exception):
+    """ Thrown when the download of an URL fails """
+
 class URLDownloader(metaclass=ABCMeta):
     """ Implementation of a class to download the contents of an URL """
 
@@ -48,18 +51,29 @@ class URLLibURLDownloader(URLDownloader):
     """ Implementation of a class to download the contents of an URL through URLLib """
 
     def _download_once(self, opener: urllib.request.OpenerDirector, request: _Request) -> bytes:
-        encoded_params = urllib.parse.urlencode(request.params) if request.params else None
-        encoded_url = f"{request.url}?{encoded_params}" if encoded_params else request.url
-        encoded_data = urllib.parse.urlencode(request.data).encode() if request.data else None
-        with opener.open(encoded_url, encoded_data, self._timeout) as url_handle:
-            return cast(bytes, url_handle.read())
+        try:
+            encoded_params = urllib.parse.urlencode(request.params) if request.params else None
+            encoded_url = f"{request.url}?{encoded_params}" if encoded_params else request.url
+            encoded_data = urllib.parse.urlencode(request.data).encode() if request.data else None
+            with opener.open(encoded_url, encoded_data, self._timeout) as url_handle:
+                return cast(bytes, url_handle.read())
+        except urllib.error.HTTPError as exception:
+            # urllib.error.HTTPError contains a file-like object and needs to be closed, see:
+            # - https://github.com/pytest-dev/pytest/issues/13308
+            # - https://docs.python.org/3.14/library/urllib.error.html
+            # Closing it avoids a ResourceWarning on e.g. Python 3.14.2.
+            with exception:
+                raise URLDownloaderException(exception.reason) from exception
+        except urllib.error.URLError as exception:
+            raise URLDownloaderException(exception.reason) from exception
+
 
     def _download(self, request: _Request) -> bytes:
         opener = self._build_opener_with_app_useragent()
         for i in range(NUM_RETRIES):
             try:
                 return self._download_once(opener, request)
-            except urllib.error.URLError as exception:
+            except URLDownloaderException as exception:
                 self._tracer.trace(f"Got exception: {exception}, retrying...")
                 time.sleep(3**i)
 
